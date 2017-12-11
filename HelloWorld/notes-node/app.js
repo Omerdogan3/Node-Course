1a4f4a983db3c47f55ff82e3f3510308195f79ef
Omer Dogan <omerdogan3@gmail.com>
2017-12-11 18:21:53 +0300

    Yargs

--- HelloWorld/notes-node/app.js
+++ HelloWorld/notes-node/app.js
@@ -2,11 +2,46 @@ console.log('Hello World');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
 // let user = os.userInfo();
 // fs.appendFile('message.txt', `Hello ${user.username} You are ${notes.age}` );
 
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+
+// console.log(process.argv);
+
+
+
+const argv = yargs.argv;
+var command = process.argv[2];
+
+console.log('Command: ' + command);
+console.log('Yargs: ',  argv);
+
+if(command === 'add'){
+    notes.addNote(argv.title, argv.body);
+}else if(command === 'list'){
+    notes.getNotes();
+}else if(command === 'read'){
+    notes.readNote(argv.title);
+}else if(command === 'remove'){
+    notes.removeNote(argv.title);
+}else{
+    console.log('Command not recognized');
+}
+
+
+
+
+
 // var res = notes.addNote();
-var sum = notes.add(3,5);
-console.log(sum);
+// var sum = notes.add(3,5);
+// console.log(sum);
+
+
+
--- HelloWorld/notes-node/notes.js
+++ HelloWorld/notes-node/notes.js
@@ -1,10 +1,33 @@
 console.log("Starting notes.js");
 
-module.exports.addNote = () =>{
-    console.log('addNote');
-    return 'New Note';
+// module.exports.addNote = () =>{
+//     console.log('addNote');
+//     return 'New Note';
+// };
+
+// module.exports.add = (a,b) =>{
+//     return a+b;
+// };
+
+var addNote = (title,body) =>{
+    console.log('Adding Note', title , body);
 };
 
-module.exports.add = (a,b) =>{
-    return a+b;
+var getNotes = () =>{
+    console.log('Getting All Notes');
+};
+
+var readNote = (title) =>{
+    console.log('Getting note', title);
+};
+
+var removeNote = (title) =>{
+    console.log('Removing note', title);
+};
+
+module.exports = {
+    addNote,
+    getNotes,
+    readNote,
+    removeNote
 };
