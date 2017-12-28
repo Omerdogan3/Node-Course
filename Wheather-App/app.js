865291af499eabdee0b47a2d70d82adbd57a315b
Omer Dogan <omerdogan3@gmail.com>
2017-12-28 20:04:09 +0300

    Http request

--- Wheather-App/app.js
+++ Wheather-App/app.js
@@ -4,5 +4,7 @@ request({
     url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error,response,body)=>{
-    console.log(body);
+    console.log(`Adress ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 });
