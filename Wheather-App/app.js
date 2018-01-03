aa0b51c45c48ad3062f73a1c19a3978e87ad7b9b
Omer Dogan <omerdogan3@gmail.com>
2018-01-03 20:55:02 +0300

    Refactoring Address Geocode

--- Wheather-App/app.js
+++ Wheather-App/app.js
@@ -1,5 +1,7 @@
 const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode.js');
+
 
 const argv = yargs
   .options({
@@ -13,6 +15,8 @@ const argv = yargs
   .help()
   .alias('help', 'h')
   .argv;
+
+geocode.geocodeAddress(argv.address);
 
 var encodedAddress = encodeURIComponent(argv.address);
 
