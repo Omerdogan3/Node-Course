6f66d85d6586d39cc5c625161be3f97e41a3c939
Omer Dogan <omerdogan3@gmail.com>
2018-01-11 18:32:06 +0300

    More Promises

--- Wheather-App/playground/promise.js
+++ Wheather-App/playground/promise.js
@@ -1,12 +1,33 @@
-var somePromise = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        // resolve('Hey. It worked!');
-        reject("Unable to fulfill promise");
-    },2500);
+var asyncAdd = (a,b) => {
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            if(typeof a === 'number' && typeof b === 'number'){
+                resolve(a+b);
+            }else{
+                reject('Arguments must be numbers');
+            }
+        },1500);
+    });
+};
+
+asyncAdd(5,'7').then((res)=>{
+    console.log(res);
+    return asyncAdd(res,33);
+}).then((res)=>{
+    console.log(res);
+}).catch((errorMessage)=>{
+    console.log(errorMessage);
 });
 
-somePromise.then((message)=>{
-    console.log('Success: ', message);
-}, (errorMessage)=>{
-    console.log('Error: ', errorMessage)
-}); 
+// var somePromise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         // resolve('Hey. It worked!');
+//         reject("Unable to fulfill promise");
+//     },2500);
+// });
+
+// somePromise.then((message)=>{
+//     console.log('Success: ', message);
+// }, (errorMessage)=>{
+//     console.log('Error: ', errorMessage)
+// }); 
