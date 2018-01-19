a0e6c59c0a3f23763ec5e7c68248796f42e040ca
Omer Dogan <omerdogan3@gmail.com>
2018-01-19 15:04:49 +0300

    Render Templates With Data

--- NodeWebServer/server.js
+++ NodeWebServer/server.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
-app.get('/', (req,res) =>{
-    // res.send('Hello Express');
-    res.send({
-        name: 'Andrew',
-        likes: ['Biking','Cities']
-    });
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
+  });
 });
 
-app.get('/about', (req,res) =>{
-    res.send('About Page');
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
-app.listen(3000);
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
