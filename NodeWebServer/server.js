e102465f46205402a7ae084456108998cae0f17b
Omer Dogan <omerdogan3@gmail.com>
2018-01-21 19:16:17 +0300

    Advanced templating
    footer.hbs e vs template belirliyorsun daha sonra onu html dosyasinin farkli yerlerinde cagirabiliyorsun.
    
    registerHelper da tekrar tekrar fonksiyon calistirmaktansa tek seferde bind ediyorsun.

--- NodeWebServer/server.js
+++ NodeWebServer/server.js
@@ -3,21 +3,24 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear()
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
