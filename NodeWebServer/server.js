9007abf59b325e56a973eff3f1b661b5359fb591
Omer Dogan <omerdogan3@gmail.com>
2018-01-22 14:44:37 +0300

    Express middleware
    next(); cagirilana  kadar sayfa yuklenmez. Cagirildigi anda render edilir.

--- NodeWebServer/server.js
+++ NodeWebServer/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log',log + '\n', (err)=>{
+    if(err){
+      console.log('Unable to append to server log.');
+    }
+  });
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear',()=>{
