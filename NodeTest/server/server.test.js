e50980656c7473dba40e370549d17c4742abdc84
Omer Dogan <omerdogan3@gmail.com>
2018-01-29 13:50:10 +0300

    Organize test cases with describe

--- NodeTest/server/server.test.js
+++ NodeTest/server/server.test.js
@@ -3,30 +3,34 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
+describe('Server', () => {
 
-// Make a new test
-// assert 200
-// Assert that you exist in users array
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Andrew',
-        age: 25
-      });
-    })
-    .end(done);
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', () => {
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Andrew',
+            age: 25
+          });
+        })
+        .end(done);
+    });
+  });
 });
