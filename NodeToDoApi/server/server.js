5c0ff15b5acf5518693d27cea029557eacdfbdf1
Omer Dogan <omerdogan3@gmail.com>
2018-02-03 17:58:26 +0300

    Mongoose Validators

--- NodeToDoApi/server/server.js
+++ NodeToDoApi/server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -25,14 +30,33 @@ var Todo = mongoose.model('Todo', {
 //   console.log('Unable to save todo')
 // });
 
-var otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
-  completedAt: 123
+// var otherTodo = new Todo({
+//   text: 'Omer',
+//   completed: true,
+//   completedAt: 123
+// });
+
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
+
+var User = mongoose.model('User',{
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
 });
 
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save', e);
+var user = new User({
+    email: 'omerdogan3@gmail.com'
 });
+
+user.save().then((doc)=>{
+    console.log('User saved', doc);
+}, (err)=>{
+    console.log('Unable to save', err);
+})
