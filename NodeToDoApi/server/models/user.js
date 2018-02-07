6ae529d362f24dcf6b3b8413b4741a3e26bac9df
Omer Dogan <omerdogan3@gmail.com>
2018-02-07 20:30:21 +0300

    Setup User

--- NodeToDoApi/server/models/user.js
+++ NodeToDoApi/server/models/user.js
@@ -1,11 +1,34 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User',{
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
+        minlength: 1,
+        unique: true,
+        validate: {
+                validator: validator.isEmail,
+                message: '{VALUE} is not a valid email'
+            },
+            
+        },
+        password: {
+            type: String,
+            require: true,
+            minlength: 6
+        },
+        tokens: [{
+            access: {
+                type: String,
+                required: true
+            },
+            token:{
+                type: String,
+                required: true
+            }
+        }]
     }
 });
 
--- NodeToDoApi/server/server.js
+++ NodeToDoApi/server/server.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -22,35 +26,90 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
     res.send({todos});
-  },(e)=>{
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
+
   Todo.findById(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
 
 module.exports = {app};
