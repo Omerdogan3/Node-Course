d7ae50ecc3b3e7fb2a4472f02346544fa7853534
Omer Dogan <omerdogan3@gmail.com>
2018-02-08 18:22:41 +0300

    hashing

---
+++ NodeToDoApi/playground/hashing.js
@@ -0,0 +1,29 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id:10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log(decoded);
+
+
+
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
