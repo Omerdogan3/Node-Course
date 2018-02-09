9358388c47cb1fa510503af60b1800ca6ddb1899
Omer Dogan <omerdogan3@gmail.com>
2018-02-09 17:29:07 +0300

    Generate Auth Token

--- NodeToDoApi/server/models/user.js
+++ NodeToDoApi/server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-var User = mongoose.model('User',{
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -29,7 +30,19 @@ var User = mongoose.model('User',{
                 required: true
             }
         }]
-    }
 });
+
+UserSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({access,token});
+    return user.save().then(()=>{
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- NodeToDoApi/server/server.js
+++ NodeToDoApi/server/server.js
@@ -101,8 +101,10 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  user.save().then((user) => {
-    res.send(user);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
