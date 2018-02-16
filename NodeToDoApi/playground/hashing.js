66a0ce88a7dff81781c59f6d9f88badf9d613ee7
Omer Dogan <omerdogan3@gmail.com>
2018-02-16 18:00:04 +0300

    Hashing Password

--- NodeToDoApi/playground/hashing.js
+++ NodeToDoApi/playground/hashing.js
@@ -1,15 +1,26 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+
+
+// var data = {
+//     id:10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 
 
